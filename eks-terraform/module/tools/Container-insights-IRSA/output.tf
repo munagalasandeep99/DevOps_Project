616436ae5dac4e4bac3d302e40e9f7045eac4fea
Sandeep Reddy Munagala <129391713+munagalasandeep99@users.noreply.github.com>
2025-05-31 10:32:20 +0200

    Delete eks-terraform/module/tools/Container-insights-IRSA directory

--- eks-terraform/module/tools/Container-insights-IRSA/output.tf
+++
@@ -1,8 +0,0 @@
-output "CONTAINER_INSIGHT_DRIVER_ROLE_ARN" {
-    value = aws_iam_role.container-insight-role.arn
-}
-
-output "AWS_CONTAINER_INSIGHT_DRIVER_ARN" {
-  value = aws_eks_addon.aws-container-insight-driver.arn
-}
-
